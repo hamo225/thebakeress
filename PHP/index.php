8ed7369b77c3b91f904658350829b455876b3ee9
Tarek <tarekipad@gmail.com>
2020-07-28 10:42:43 +0200

    First Commit

---
+++ PHP/index.php
@@ -0,0 +1,35 @@
+<?php
+// Check for empty fields - Validate the data
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['tel'])     ||
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+      header('location:errorpage.html');  
+      return false;
+   }
+   
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$phone = $_POST['tel'];
+// $subject = $_POST['subject'];
+$message = $_POST['message'];
+   
+// Create the email and send the message
+$to = 'tarekipad@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Subject: Contact form Submission from $name" ;
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone Number: $phone
+\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";   
+
+if(mail($to,$email_subject,$email_body,$headers)){
+//    header('location:thanks.html'); 
+echo 'thanks'; 
+   exit;
+} else{
+   header('location:errorpage.html');  
+
+}        
+?>
--- README.md
+++
@@ -1,4 +0,0 @@
-# thebakeress
-A bakers merchant online shop
-
-Currently being re-worked with SASS. 
