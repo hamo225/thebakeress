362df647309413d1410444e294cab74dbb192404
Tarek <tarekipad@gmail.com>
2020-11-24 18:10:26 +0100

    starts setting mobile responsive

--- JS/index.js
+++ JS/index.js
@@ -1,2 +1,19 @@
 var video = document.getElementById("my-video");
 video.playbackRate = 0.9;
+
+const nyctopping = document.querySelector(".nyCheesecaketoppings");
+const spanishtopping = document.querySelector(".spanishCheesecaketoppings");
+const nyccheescakeOrderNumber = document.querySelector("#nyccheesecake");
+const spanishCheeseCakeOrderNumber = document.querySelector(
+  "#spanishcheesecake"
+);
+nyctopping.style.display = "none";
+spanishtopping.style.display = "none";
+
+nyccheescakeOrderNumber.addEventListener("click", function () {
+  nyctopping.style.display = "block";
+});
+
+spanishCheeseCakeOrderNumber.addEventListener("click", function () {
+  spanishtopping.style.display = "block";
+});
