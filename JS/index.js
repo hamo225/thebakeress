1ace6bd360ed600e153950f1fc185334883d8871
Tarek <tarekipad@gmail.com>
2020-11-25 17:40:40 +0100

    setting php validation and form sending

--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,4 @@
-var video = document.getElementById("my-video");
-video.playbackRate = 0.9;
+// var video = document.getElementById("my-video");
+// video.playbackRate = 0.9;
 
-const nyctopping = document.querySelector(".nyCheesecaketoppings");
-const spanishtopping = document.querySelector(".spanishCheesecaketoppings");
-const nyccheescakeOrderNumber = document.querySelector("#nyccheesecake");
-const spanishCheeseCakeOrderNumber = document.querySelector(
-  "#spanishcheesecake"
-);
-nyctopping.style.display = "none";
-spanishtopping.style.display = "none";
-
-nyccheescakeOrderNumber.addEventListener("click", function () {
-  nyctopping.style.display = "block";
-});
-
-spanishCheeseCakeOrderNumber.addEventListener("click", function () {
-  spanishtopping.style.display = "block";
-});
+const navLink = document.querySelector(".navigation__link");
--- PHP/order.php
+++ PHP/order.php
@@ -1,8 +1,9 @@
 <?php
 // Check for empty fields - Validate the data
+
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
-   empty($_POST['tel'])     ||
+   empty($_POST['subject'])     ||
    empty($_POST['message'])   ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
@@ -12,21 +13,20 @@ if(empty($_POST['name'])      ||
    
 $name = $_POST['name'];
 $email_address = $_POST['email'];
-$phone = $_POST['tel'];
-// $subject = $_POST['subject'];
+$phone = $_POST['telphonenumber'];
+$subject = $_POST['subject'];
 $message = $_POST['message'];
    
 // Create the email and send the message
-$to = 'tarekipad@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Subject: Contact form Submission from $name" ;
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone Number: $phone
+$to = 'mashalashelter@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Subject:  $subject" ;
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nSubject: $subject\n\nName: $name\n\nEmail: $email_address\n\nPhone Number: $phone
 \n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 
 if(mail($to,$email_subject,$email_body,$headers)){
-//    header('location:thanks.html'); 
-echo 'thanks'; 
+   header('location:thanks.html');  
    exit;
 } else{
    header('location:errorpage.html');  
